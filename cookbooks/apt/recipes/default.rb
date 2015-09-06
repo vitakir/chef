84ccbc9a2e327c5c46d833b5431a487d9136160e
Vitaly Kir <vkir@heartflow.com>
2015-09-06 15:40:17 -0400

    Initial

---
+++ cookbooks/apt/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: apt
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "yum update" do
+    command "yum update -y"
+end
